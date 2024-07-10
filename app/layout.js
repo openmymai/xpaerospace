e4dc3ab4744dc48fc9e1f0b862f2bf28335f893f
openmymai <maicmi@me.com>
2024-07-10 11:09:50 +0700

    Fix Capabilities Dropdown Size

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className={noto.className}>
-        <Navigation />
+          <Navigation />
         {children}
         <Contact />
         <Footer />
