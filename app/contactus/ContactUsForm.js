03e2c851a7e0b641102a37d209e4875d17a37354
openmymai <maicmi@me.com>
2024-07-11 20:41:15 +0700

    Fix all class to className and for to htmlFor

--- app/contactus/ContactUsForm.js
+++ app/contactus/ContactUsForm.js
@@ -44,204 +44,204 @@ export default function ContactUsForm() {
                 onSubmit={onSubmit}
                 className='row mb-3'
               >
-                <div class='form__group'>
+                <div className='form__group'>
                   <input
                     type='text'
-                    class='form__input'
+                    className='form__input'
                     placeholder='คำนำหน้า'
                     id='title'
                     required
                   />
                   <label
-                    for='title'
-                    class='form__label'
+                    htmlFor='title'
+                    className='form__label'
                   >
                     คำนำหน้า
                   </label>
                 </div>
-                <div class='form__group'>
+                <div className='form__group'>
                   <input
                     type='text'
-                    class='form__input'
+                    className='form__input'
                     placeholder='ชื่อ'
                     id='firstname'
                     required
                   />
                   <label
-                    for='firstname'
-                    class='form__label'
+                    htmlFor='firstname'
+                    className='form__label'
                   >
                     ชื่อ
                   </label>
                 </div>
-                <div class='form__group'>
+                <div className='form__group'>
                   <input
                     type='text'
-                    class='form__input'
+                    className='form__input'
                     placeholder='นามสกุล'
                     id='lastname'
                     required
                   />
                   <label
-                    for='lastname'
-                    class='form__label'
+                    htmlFor='lastname'
+                    className='form__label'
                   >
                     นามสกุล
                   </label>
                 </div>
 
-                <div class='form__group'>
+                <div className='form__group'>
                   <input
                     type='text'
-                    class='form__input'
+                    className='form__input'
                     placeholder='บริษัท'
                     id='company'
                     required
                   />
                   <label
-                    for='company'
-                    class='form__label'
+                    htmlFor='company'
+                    className='form__label'
                   >
                     บริษัท
                   </label>
                 </div>
 
-                <div class='form__group'>
+                <div className='form__group'>
                   <input
                     type='text'
-                    class='form__input'
+                    className='form__input'
                     placeholder='ที่อยู่'
                     id='address'
                     required
                   />
                   <label
-                    for='address'
-                    class='form__label'
+                    htmlFor='address'
+                    className='form__label'
                   >
                     ที่อยู่
                   </label>
                 </div>
 
-                <div class='form__group'>
+                <div className='form__group'>
                   <input
                     type='text'
-                    class='form__input'
+                    className='form__input'
                     placeholder='จังหวัด'
                     id='province'
                     required
                   />
                   <label
-                    for='province'
-                    class='form__label'
+                    htmlFor='province'
+                    className='form__label'
                   >
                     จังหวัด
                   </label>
                 </div>
 
-                <div class='form__group'>
+                <div className='form__group'>
                   <input
                     type='text'
-                    class='form__input'
+                    className='form__input'
                     placeholder='รหัสไปรษณีย์'
                     id='postal'
                     required
                   />
                   <label
-                    for='postal'
-                    class='form__label'
+                    htmlFor='postal'
+                    className='form__label'
                   >
                     รหัสไปรษณีย์
                   </label>
                 </div>
 
-                <div class='form__group'>
+                <div className='form__group'>
                   <input
                     type='text'
-                    class='form__input'
+                    className='form__input'
                     placeholder='โทร'
                     id='phone'
                     required
                   />
                   <label
-                    for='phone'
-                    class='form__label'
+                    htmlFor='phone'
+                    className='form__label'
                   >
                     โทร
                   </label>
                 </div>
 
-                <div class='form__group'>
+                <div className='form__group'>
                   <input
                     type='text'
-                    class='form__input'
+                    className='form__input'
                     placeholder='อีเมล์'
                     id='email'
                     required
                   />
                   <label
-                    for='email'
-                    class='form__label'
+                    htmlFor='email'
+                    className='form__label'
                   >
                     อีเมล์
                   </label>
                 </div>
 
-                <label class='text'>ลักษณะโครงการ</label>
+                <label className='text'>ลักษณะโครงการ</label>
 
-                <div class='form__group u-margin-bottom-medium'>
-                  <div class='form__checkbox-group'>
+                <div className='form__group u-margin-bottom-medium'>
+                  <div className='form__checkbox-group'>
                     <input
                       type='checkbox'
-                      class='form__checkbox-input'
+                      className='form__checkbox-input'
                       id='military'
                       name='size'
                     />
                     <label
-                      for='military'
-                      class='form__checkbox-label'
+                      htmlFor='military'
+                      className='form__checkbox-label'
                     >
-                      <span class='form__checkbox-button'></span>
+                      <span className='form__checkbox-button'></span>
                       การทหาร
                     </label>
                   </div>
 
-                  <div class='form__checkbox-group'>
+                  <div className='form__checkbox-group'>
                     <input
                       type='checkbox'
-                      class='form__checkbox-input'
+                      className='form__checkbox-input'
                       id='commercial'
                       name='size'
                     />
                     <label
-                      for='commercial'
-                      class='form__checkbox-label'
+                      htmlFor='commercial'
+                      className='form__checkbox-label'
                     >
-                      <span class='form__checkbox-button'></span>
+                      <span className='form__checkbox-button'></span>
                       พาณิชย์
                     </label>
                   </div>
 
-                  <div class='form__checkbox-group'>
+                  <div className='form__checkbox-group'>
                     <input
                       type='checkbox'
-                      class='form__checkbox-input'
+                      className='form__checkbox-input'
                       id='private'
                       name='size'
                     />
                     <label
-                      for='private'
-                      class='form__checkbox-label'
+                      htmlFor='private'
+                      className='form__checkbox-label'
                     >
-                      <span class='form__checkbox-button'></span>
+                      <span className='form__checkbox-button'></span>
                       เอกชน
                     </label>
                   </div>
                 </div>
 
-                <label class='text'>รายละเอียด</label>
-                <div class='form__group'>
+                <label className='text'>รายละเอียด</label>
+                <div className='form__group'>
                   <textarea
-                    class='form__input'
+                    className='form__input'
                     placeholder='โปรดระบุว่าคำขอนี้มีไว้สำหรับการจัดซื้อจัดจ้าง, ระบบการบิน,
                      การปรับปรุงห้องนักบิน, การสร้างต้นแบบ, การทดสอบการบิน, การฝึกนักบิน, และ/หรือ การฝึกอบรมช่างเครื่อง'
                     id='description'
@@ -249,8 +249,8 @@ export default function ContactUsForm() {
                     required
                   />
                   <label
-                    for='description'
-                    class='form__label'
+                    htmlFor='description'
+                    className='form__label'
                   >
                     รายละเอียด
                   </label>
