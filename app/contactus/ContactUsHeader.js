6f8a603eb7a2f87b19c2fa4748c03b76fe91928e
openmymai <maicmi@me.com>
2024-07-11 13:41:39 +0700

    Fix Contact Us Form Padding

--- app/contactus/ContactUsHeader.js
+++ app/contactus/ContactUsHeader.js
@@ -3,7 +3,7 @@ export default function ContactUsHeader() {
     <div className='contactuspage'>
       <div className='contactuspage__text-box'>
         <h1 className='heading-primary'>
-          <span className='heading-primary--main'>Contact</span>
+          <span className='heading-primary--main'>ติดต่อ</span>
           <span className='heading-primary--sub'>XPAT</span>
         </h1>
       </div>
