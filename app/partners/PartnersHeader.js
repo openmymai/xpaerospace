f2aa88ea7c9152c5b9ce71cfbb3b5c9b846de26c
openmymai <maicmi@me.com>
2024-07-24 17:04:09 +0700

    Fix Partner Header Title

--- app/partners/PartnersHeader.js
+++ app/partners/PartnersHeader.js
@@ -3,7 +3,7 @@ export default function PartnersHeader() {
     <div className='partnerspage'>
       <div className='partnerspage__text-box'>
         <h1 className='heading-primary'>
-          <span className='heading-primary--main'>คู่ค้าของเรา</span>
+          <span className='heading-primary--main'>พันธมิตรของเรา</span>
         </h1>
       </div>
     </div>
